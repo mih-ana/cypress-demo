ef57e8c3319f7743e81ea49a3fd0997100662693
Ana Mihajloska <mihajloska.ana@gmail.com>
2020-12-14 09:23:46 +0100

    Replaced signup test case with signin test case

---
+++ cypress/integration/examples/SignIn/PositiveTests/valid_signin.spec.js
@@ -0,0 +1,16 @@
+/// <reference types="cypress" />
+
+describe('SignIn - Positive Tests', () => {
+    it('Valid SignIn', () => {
+        cy.visit('https://www.saucedemo.com/')
+        cy.wait(5000)
+        cy.get('#user-name').click().type('standard_user').should('have.value', 'standard_user')
+        cy.wait(2000)
+        cy.get('#password').click().type('secret_sauce').should('have.value', 'secret_sauce')
+        cy.wait(2000)
+        cy.get('#login-button').click()
+        cy.location().should((location) => {
+            expect(location.href).to.eq('https://www.saucedemo.com/inventory.html')
+        })
+    })
+})
