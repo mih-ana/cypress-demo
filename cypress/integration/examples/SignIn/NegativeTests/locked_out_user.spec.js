2add984823849a7b507918b48f904cb022c3d40d
Ana Mihajloska <mihajloska.ana@gmail.com>
2020-12-14 10:21:20 +0100

    Added initial assertions

--- cypress/integration/examples/SignIn/NegativeTests/locked_out_user.spec.js
+++ cypress/integration/examples/SignIn/NegativeTests/locked_out_user.spec.js
@@ -4,6 +4,9 @@ describe('SignIn - Negative Tests', () => {
     it('Valid SignIn', () => {
         cy.visit('https://www.saucedemo.com/')
         cy.wait(5000)
+        cy.location().should((location) => {
+            expect(location.href).to.eq('https://www.saucedemo.com/')
+        })
         cy.get('#user-name').click().type('locked_out_user').should('have.value', 'locked_out_user')
         cy.wait(2000)
         cy.get('#password').click().type('secret_sauce').should('have.value', 'secret_sauce')
--- cypress/integration/examples/SignIn/PositiveTests/valid_signin.spec.js
+++ cypress/integration/examples/SignIn/PositiveTests/valid_signin.spec.js
@@ -4,6 +4,9 @@ describe('SignIn - Positive Tests', () => {
     it('Valid SignIn', () => {
         cy.visit('https://www.saucedemo.com/')
         cy.wait(5000)
+        cy.location().should((location) => {
+            expect(location.href).to.eq('https://www.saucedemo.com/')
+        })
         cy.get('#user-name').click().type('standard_user').should('have.value', 'standard_user')
         cy.wait(2000)
         cy.get('#password').click().type('secret_sauce').should('have.value', 'secret_sauce')
