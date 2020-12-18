2c8339bdd15f9b5514963bc6701bcdef25b0ea02
Ana Mihajloska <mihajloska.ana@gmail.com>
2020-12-18 10:33:48 +0100

    Tidying up the Navigation test suite

--- cypress/integration/examples/MainMenuNavigations/PositiveTests/navigation.spec.js
+++ cypress/integration/examples/MainMenuNavigations/PositiveTests/navigation.spec.js
@@ -1,7 +1,7 @@
 /// <reference types="cypress" />
 
- //Preconditions: The user has signed in
- before(() => {
+//Preconditions: The user has signed in
+before(() => {
     it('Valid SignIn', () => {
         cy.visit('https://www.saucedemo.com/')
         cy.wait(5000)
@@ -17,79 +17,76 @@
             expect(location.href).to.eq('https://www.saucedemo.com/inventory.html')
         })
     })
-})
 
-describe('MainMenuNavigations - Positive Tests', () => {
-    //Get the navbar and click on it before each test case
-    beforeEach(() => {
-        cy.visit('https://www.saucedemo.com/inventory.html')
-        cy.wait(2000)
-        cy.get('#menu_button_container > div > div:nth-child(3) > div > button').contains('Open Menu').click()
-        cy.wait(2000)
-        cy.get('.bm-menu').should('exist')
-    })
+    describe('MainMenuNavigations - Positive Tests', () => {
+        //Get the navbar and click on it before each test case
+        beforeEach(() => {
+            cy.visit('https://www.saucedemo.com/inventory.html')
+            cy.wait(2000)
+            cy.get('#menu_button_container > div > div:nth-child(3) > div > button').contains('Open Menu').click()
+            cy.wait(2000)
+            cy.get('.bm-menu').should('exist')
+        })
 
-    //Reload the page
-    // it('Reload the page', () => {
-    //     cy.reload()
-    // })
-
-    //Click on the All Items menu item
-    // it('Click on the All Items menu item', () => {
-    //     cy.get('#inventory_sidebar_link').click()
-    //     cy.wait(2000)
-    //     cy.location().should((location) => {
-    //         expect(location.href).to.eq('https://www.saucedemo.com/inventory.html')
-    //     })
-    // })
-
-    //Click on the About menu item, then go back - The user should be redirected to the Inventory page
-    // it('Click on the About menu item', () => {
-    //     cy.get('#about_sidebar_link').click()
-    //     cy.wait(2000)
-    //     cy.url().should('eq','https://saucelabs.com/')
-    //     cy.go('back')
-    //     cy.wait(2000)
-    //     cy.url().should('eq', 'https://www.saucedemo.com/inventory.html')
-    //})
-
-    //Click on the About menu item, then go back, go forward and go back again - The user should be redirected to the Inventory page
-    // it('Click on the About menu item', () => {
-    //     cy.get('#about_sidebar_link').click()
-    //     cy.wait(2000)
-    //     cy.url().should('eq', 'https://saucelabs.com/')
-    //     cy.go('back')
-    //     cy.wait(2000)
-    //     cy.url().should('eq', 'https://www.saucedemo.com/inventory.html')
-    //     cy.go('forward')
-    //     cy.wait(2000)
-    //     cy.url().should('eq', 'https://saucelabs.com/')
-    //     cy.go('back')
-    //     cy.wait(2000)
-    //     cy.url().should('eq', 'https://www.saucedemo.com/inventory.html')
-    // })
-
-    //     //Close the navbar by clicking on the 'x' button - The navbar should be closed
-        // it('Close the navbar by clicking on the button', () => {
-        //     cy.get('#menu_button_container > div > div.bm-menu-wrap > div:nth-child(2) > div > button').click()
-        //     cy.wait(2000)
-        //     cy.get('.bm-menu').should('not.be.visible')
+        //Reload the page
+        // it('Reload the page', () => {
+        //     cy.reload()
         // })
 
-    //     //Close the navbar by clicking anywhere on the page - The navbar should be closed
-        // it('Close the navbar by clicking anywhere on the page', () => {
-        //     cy.get('#menu_button_container > div > div.bm-overlay').click()
-        //     cy.wait(2000)
-        //     cy.get('.bm-menu').should('not.be.visible')
-        // })
+        //Click on the All Items menu item
+        it('Click on the All Items menu item', () => {
+            cy.get('#inventory_sidebar_link').click()
+            cy.wait(2000)
+            cy.location().should((location) => {
+                expect(location.href).to.eq('https://www.saucedemo.com/inventory.html')
+            })
+        })
 
-    //     //Click on the Logout menu item - The user should be logged out
+        //Click on the About menu item, then go back - The user should be redirected to the Inventory page
+        it('Click on the About menu item', () => {
+            cy.get('#about_sidebar_link').click()
+            cy.wait(2000)
+            cy.url().should('eq', 'https://saucelabs.com/')
+            cy.go('back')
+            cy.wait(2000)
+            cy.url().should('eq', 'https://www.saucedemo.com/inventory.html')
+        })
+
+        //Click on the About menu item, then go back, go forward and go back again - The user should be redirected to the Inventory page
+        it('Click on the About menu item', () => {
+            cy.get('#about_sidebar_link').click()
+            cy.wait(2000)
+            cy.url().should('eq', 'https://saucelabs.com/')
+            cy.go('back')
+            cy.wait(2000)
+            cy.url().should('eq', 'https://www.saucedemo.com/inventory.html')
+            cy.go('forward')
+            cy.wait(2000)
+            cy.url().should('eq', 'https://saucelabs.com/')
+            cy.go('back')
+            cy.wait(2000)
+            cy.url().should('eq', 'https://www.saucedemo.com/inventory.html')
+        })
+
+        //Close the navbar by clicking on the 'x' button - The navbar should be closed
+        it('Close the navbar by clicking on the button', () => {
+            cy.get('#menu_button_container > div > div.bm-menu-wrap > div:nth-child(2) > div > button').click()
+            cy.wait(2000)
+            cy.get('.bm-menu').should('not.be.visible')
+        })
+
+        //Close the navbar by clicking anywhere on the page - The navbar should be closed
+        it('Close the navbar by clicking anywhere on the page', () => {
+            cy.get('#menu_button_container > div > div.bm-overlay').click()
+            cy.wait(2000)
+            cy.get('.bm-menu').should('not.be.visible')
+        })
+
+        //Click on the Logout menu item - The user should be logged out
         it('Click on the Logout menu item', () => {
             cy.get('#logout_sidebar_link').click()
             cy.wait(2000)
-            cy.url().should('eq','https://www.saucedemo.com/index.html')
+            cy.url().should('eq', 'https://www.saucedemo.com/index.html')
         })
-  //  })
-
-
+    })
 })
